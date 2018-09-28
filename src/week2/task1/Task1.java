a56fbf2cc206996db0eb91aaf3c80d3e3cdbad83
tutt1999nd1 <43142674+tutt1999nd1@users.noreply.github.com>
2018-09-28 18:06:50 +0700

    sua loi ko auto

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -4,17 +4,11 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        a=Math.abs(a);
-        b=Math.abs(b);
-        if(b==0)return a;
-
-        return gcd(b,a%b);
+        return 0;
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        if(n==0)return 0;
-        else if(n==1)return 1;
-        return fibonacci(n-1)+fibonacci(n-2);
+        return 0;
     }
 }
